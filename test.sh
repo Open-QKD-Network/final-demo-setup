a2088c439ea0b68833161e54b204457ad8505809
JohnBish <johnbrucebishop@gmail.com>
2022-12-06 15:19:01 -0500

    Remote test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo -e "\nSetting up dummy drivers..."
-for s in "can1 A" "can2 B" "can3 C" "uk1 D" "uk2 E"
-do
-    set -- $s # convert tuple to args
-    ssh -F ./ssh/config $1 "cd ~/build-cqptoolkit && ./setupTest3$2.sh"
-done
